03384f0662a816881d7201f8cd08f094f3fe7ed7
Rudra Kumar <rudraroop.33@gmail.com>
2025-08-13 00:45:12 +0530

    updating swagger congig

--- src/main/java/com/sanjayfirst/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sanjayfirst/blog/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sanjayfirst.blog.exception;
 
 
 import com.sanjayfirst.blog.payloads.ApiResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
